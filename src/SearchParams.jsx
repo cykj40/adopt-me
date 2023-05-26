f003dcd8181038b14f741ce3f08893f3cedacc77
cykj40 <102045473+cykj40@users.noreply.github.com>
2023-05-26 09:19:16 -0400

    we did so much  in the portion of the front end masters its tool long to put in a commit message

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,77 +1,76 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 import useBreedList from "./useBreedList";
+import fetchSearch from "./fetchSearch";
 import Results from "./Results";
 
 const SearchParams = () => {
-    const [location, setLocation] = useState("");
+    const [requestParams, setRequestParams] = useState({
+        location: "",
+        animal: "",
+        breed: "",
+    });
     const [animal, setAnimal] = useState("");
-    const [breed, setBreed] = useState("");
-    const [pets, setPets] = useState([]);
+
     const [breeds] = useBreedList(animal);
 
-    useEffect(() => {
-        requestPets();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []); // eslint-disable-next-line react-hooks/exhaustive-deps
+    const results = useQuery(["search", requestParams], fetchSearch,)
+    const pets = results?.data?.pets ?? [];
 
-    async function requestPets() {
-        const res = await fetch(
-            `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
-        );
-        const json = await res.json();
 
-        setPets(json.pets);
-    }
 
     return (
         <div className="search-params">
-            <form onSubmit={(e) =>{
+            <form onSubmit={(e) => {
                 e.preventDefault();
-                requestPets();
+                const formData = new FormData(e.target);
+                const obj = {
+                    animal: formData.get("animal") ?? "",
+                    breed: formData.get("breed") ?? "",
+                    location: formData.get("location") ?? "",
+                };
+                setRequestParams(obj);
             }}
             >
                 <label htmlFor="location">
                     Location
-                    <input 
-                    onChange={e => setLocation(e.target.value)} 
-                    id="location" 
-                    value={location} 
-                    placeholder="Location" 
+                    <input
+                        name="location"
+                        id="location"
+                        placeholder="Location"
                     />
                 </label>
                 <label htmlFor="animal">
                     Animal
                     <select
-                    id="animal"
-                    value={animal}
-                    onChange={(e) => {
-                        setAnimal(e.target.value);
-                        setBreed("");
-                    }}
+                        id="animal"
+                        value={animal}
+                        onChange={(e) => {
+                            setAnimal(e.target.value);
+
+                        }}
                     >
                         <option />
                         {ANIMALS.map((animal) => (
                             <option key={animal}>{animal}</option>
                         ))}
                     </select>
-                 </label>
-                    <label htmlFor="breed">
-                        Breed
-                        <select
+                </label>
+                <label htmlFor="breed">
+                    Breed
+                    <select
                         id="breed"
                         disabled={breeds.length === 0}
-                        value={breed}
-                        onChange={(e) => {
-                            setBreed(e.target.value);
-                        }}
-                        >
-                            <option />
-                            {breeds.map((breed) => (
-                                <option key={breed}>{breed}</option>
-                            ))}
-                        </select>
-                    </label>
+                        name="breed"
+
+                    >
+                        <option />
+                        {breeds.map((breed) => (
+                            <option key={breed}>{breed}</option>
+                        ))}
+                    </select>
+                </label>
                 <button>Submit</button>
             </form>
             <Results pets={pets} />
